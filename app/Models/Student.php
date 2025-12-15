d3f0e36f68ea6c5fd362235db359b07d412c183d
Athirah <athirahafwan2@gmail.com>
2025-12-15 23:55:30 +0800

    Add belongsToMany relationships

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Student extends Model
 {
-    //
+    protected $fillable = ['name','email','matricNo'];
+
+    public function courses()
+    {
+        return $this->belongsToMany(Course::class)->withTimestamps();
+    }
+
 }
