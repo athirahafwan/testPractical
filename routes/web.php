e29d2fe4e865c0693057262c13835373a2f3e31f
Athirah <athirahafwan2@gmail.com>
2025-12-16 00:09:39 +0800

    Add routes for enroll and drop (many-to-many)

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StudentController;
+use App\Http\Controllers\CourseController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('students', StudentController::class);
+Route::resource('courses', CourseController::class);
+
+// enroll + drop
+Route::post('students/{student}/enroll', [StudentController::class, 'enroll'])
+    ->name('students.enroll');
+
+Route::delete('students/{student}/drop/{course}', [StudentController::class, 'drop'])
+    ->name('students.drop');
